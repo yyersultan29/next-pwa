2938c3ef4fea3ac3dc8a7a40d84c0dd1c294ee2d
Yersultan <129984720+yyersultan29@users.noreply.github.com>
2025-02-26 22:10:26 +0500

    first commit

---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Next.js PWA',
+    short_name: 'NextPWA',
+    description: 'A Progressive Web App built with Next.js by yers',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#fff',
+    icons: [
+      {
+        src: '/icon512_rounded.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon512_maskable.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
