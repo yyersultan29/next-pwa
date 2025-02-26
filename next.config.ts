d9e258b461d25ade31bd4fa8551685e108ad5a53
Yersultan <129984720+yyersultan29@users.noreply.github.com>
2025-02-26 23:31:16 +0500

    next-pwa done

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import Pwa from 'next-pwa'
 
+const withPWA = Pwa({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+}
 
-export default nextConfig;
+export default withPWA(nextConfig)
